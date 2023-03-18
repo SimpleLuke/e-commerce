a2c6249875616f2b73a6e14f7d37ba26324524d1
Luke Lai <lukelai.dev@gmail.com>
2023-03-18 19:26:01 +0000

    Add CategoryItem component test

--- src/components/category-item/Category-item.js
+++ src/components/category-item/Category-item.js
@@ -2,17 +2,25 @@ const CategoryItem = ({ categories }) => {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-xl py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        <h2
+          data-test="title"
+          className="text-2xl font-bold tracking-tight text-gray-900"
+        >
           Shop by Collection
         </h2>
-        <p className="mt-4 text-base text-gray-500">
+        <p data-test="text" className="mt-4 text-base text-gray-500">
           Each season, we collaborate with world-class designers to create a
           collection inspired by the modern world.
         </p>
 
         <div className="mt-10 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-8 lg:space-y-0">
           {categories.map((category) => (
-            <a key={category.name} href={""} className="group block">
+            <a
+              data-test="category-item"
+              key={category.name}
+              href={""}
+              className="group block"
+            >
               <div
                 aria-hidden="true"
                 className="aspect-w-3 aspect-h-2 overflow-hidden rounded-lg group-hover:opacity-75 lg:aspect-w-5 lg:aspect-h-6"
