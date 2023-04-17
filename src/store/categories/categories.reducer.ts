debfb498782f423f24e2fe8e4dc10ff4df8381e2
Luke Lai <lukelai.dev@gmail.com>
2023-04-17 17:47:50 +0100

    Convert Cart reducer to TypeScript

--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -13,7 +13,7 @@ export const CATEGORIES_INITIAL_STATE:CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as AnyAction
+  action : AnyAction
 ):CategoriesState => {
   if(setCategoriesMap.match(action)){
     return { ...state, categoriesMap: action.payload };
