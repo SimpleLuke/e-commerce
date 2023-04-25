c385dbc12fb732fe3bf4ccc6bdcd30c4a9bac078
Luke Lai <lukelai.dev@gmail.com>
2023-04-25 13:11:36 +0100

    Remove unused files

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const postcss = require("postcss");
-
-module.exports = {
-  plugins: [postcss, autoprefixer, tailwindcss("./tailwind.config.js")],
-};
