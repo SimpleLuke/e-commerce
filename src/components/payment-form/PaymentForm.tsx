bded669d2ea186a25e335bd544086d045c1a1cc1
Luke Lai <lukelai.dev@gmail.com>
2023-05-02 13:46:08 +0100

    Remove delivery options

--- src/components/payment-form/PaymentForm.tsx
+++ src/components/payment-form/PaymentForm.tsx
@@ -23,29 +23,10 @@ import {
 
 import { CartItem } from "../../store/cart/cart.types";
 
-import { RadioGroup } from "@headlessui/react";
-import {
-  CheckCircleIcon,
-  TrashIcon,
-  PlusIcon,
-  MinusIcon,
-} from "@heroicons/react/20/solid";
+import { TrashIcon, PlusIcon, MinusIcon } from "@heroicons/react/20/solid";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { useNavigate } from "react-router-dom";
-const deliveryMethods = [
-  {
-    id: 1,
-    title: "Standard",
-    turnaround: "4–10 business days",
-    price: "$5.00",
-  },
-  { id: 2, title: "Express", turnaround: "2–5 business days", price: "$16.00" },
-];
 const paymentMethods = [{ id: "credit-card", title: "Credit card" }];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
 
 const ifValidCardElement = (
   card: StripeCardNumberElement | null
@@ -56,9 +37,6 @@ const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const dispatch = useDispatch();
-  const [selectedDeliveryMethod, setSelectedDeliveryMethod] = useState(
-    deliveryMethods[0]
-  );
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
   const currentUser = useSelector(selectCurrentUser);
@@ -327,75 +305,6 @@ const PaymentForm = () => {
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="mt-10 border-t border-gray-200 pt-10">
-          <RadioGroup
-            value={selectedDeliveryMethod}
-            onChange={setSelectedDeliveryMethod}
-            name="delivery-method"
-          >
-            <RadioGroup.Label className="text-lg font-medium text-gray-900">
-              Delivery method
-            </RadioGroup.Label>
-
-            <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-              {deliveryMethods.map((deliveryMethod) => (
-                <RadioGroup.Option
-                  key={deliveryMethod.id}
-                  value={deliveryMethod}
-                  className={({ checked, active }) =>
-                    classNames(
-                      checked ? "border-transparent" : "border-gray-300",
-                      active ? "ring-2 ring-indigo-500" : "",
-                      "relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm focus:outline-none"
-                    )
-                  }
-                >
-                  {({ checked, active }) => (
-                    <>
-                      <span className="flex flex-1">
-                        <span className="flex flex-col">
-                          <RadioGroup.Label
-                            as="span"
-                            className="block text-sm font-medium text-gray-900"
-                          >
-                            {deliveryMethod.title}
-                          </RadioGroup.Label>
-                          <RadioGroup.Description
-                            as="span"
-                            className="mt-1 flex items-center text-sm text-gray-500"
-                          >
-                            {deliveryMethod.turnaround}
-                          </RadioGroup.Description>
-                          <RadioGroup.Description
-                            as="span"
-                            className="mt-6 text-sm font-medium text-gray-900"
-                          >
-                            {deliveryMethod.price}
-                          </RadioGroup.Description>
-                        </span>
-                      </span>
-                      {checked ? (
-                        <CheckCircleIcon
-                          className="h-5 w-5 text-indigo-600"
-                          aria-hidden="true"
-                        />
-                      ) : null}
-                      <span
-                        className={classNames(
-                          active ? "border" : "border-2",
-                          checked ? "border-indigo-500" : "border-transparent",
-                          "pointer-events-none absolute -inset-px rounded-lg"
-                        )}
-                        aria-hidden="true"
-                      />
-                    </>
-                  )}
-                </RadioGroup.Option>
-              ))}
-            </div>
-          </RadioGroup>
         </div>
 
         {/* Payment */}
